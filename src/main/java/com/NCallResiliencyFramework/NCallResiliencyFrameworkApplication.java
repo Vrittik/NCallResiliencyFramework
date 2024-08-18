0126282741fa49d5ae1d1214f0d5549a12c01232
vrittiksharma <vrittiksharma2@gmail.com>
2024-08-19 01:46:07 +0530

    initial push

---
+++ src/main/java/com/NCallResiliencyFramework/NCallResiliencyFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.NCallResiliencyFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NCallResiliencyFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NCallResiliencyFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NCallResiliencyFramework/NCallResiliencyFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NCallResiliencyFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NCallResiliencyFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
